41b899b4f386499f640095ad7c8113bea47cb00d
Navedkhan06 <nkhan25790@gmail.com>
2025-09-29 18:42:04 +0530

    day 9

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,49 @@
 // card
 
 export const App = () => {
-  return( <div>
-    <img src="bg1.jpg" alt="image" width="40%" height="40%" />
-    <h2>Name: Raahi Bus </h2>
-    <h3>Rating:8.2</h3>
-    <p>Summary: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque qui laborum explicabo dignissimos, illum, repellendus neque impedit ab esse iste assumenda at quisquam, quo debitis.</p>
-  </div>
-  )
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="doremon.jpg" alt="image" width="30%" height="40%" />
+        </div>
+        <h2>Cartoon Name: Doremon </h2>
+        <h3>Rating:8.2</h3>
+        <p>Summary: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque qui laborum explicabo dignissimos, illum, repellendus neque impedit ab esse iste assumenda at quisquam, quo debitis.</p>
+      </div>
+
+      <div>
+        <div>
+          <img src="chota_bheem.jpg" alt="image" width="30%" height="40%" />
+        </div>
+        <h2>Cartoon Name: Chota Bheem </h2>
+        <h3>Rating:9.2</h3>
+        <p>Summary: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque qui laborum explicabo dignissimos, illum, repellendus neque impedit ab esse iste assumenda at quisquam, quo debitis.</p>
+      </div>
+
+      {/* <CartoonSeries></CartoonSeries>  */}
+      <CartoonSeries/>
+
+      <div>
+        <div>
+          <img src="ninja_hattori.jpg" alt="image" width="30%" height="40%" />
+        </div>
+        <h2>Cartoon Name: Ninja Hattori </h2>
+        <h3>Rating:6.2</h3>
+        <p>Summary: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque qui laborum explicabo dignissimos, illum, repellendus neque impedit ab esse iste assumenda at quisquam, quo debitis.</p>
+      </div>
+
+      <div>
+        <div>
+          <img src="ben_10.jpg" alt="image" width="30%" height="40%" />
+        </div>
+        <h2>Cartoon Name: Ben 10 </h2>
+        <h3>Rating:7.2</h3>
+        <p>Summary: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque qui laborum explicabo dignissimos, illum, repellendus neque impedit ab esse iste assumenda at quisquam, quo debitis.</p>
+      </div>
+
+    </div>
+  );
 };
 
 
@@ -37,3 +73,17 @@ export const App = () => {
 // Every JSX tag needs to be closed. You can use
 // self-closing tags for elements that don't have children
 // e.g. <img scr="url" />
+
+
+const CartoonSeries = () => {
+  return (
+    <div>
+      <div>
+        <img src="doremon.jpg" alt="image" width="30%" height="40%" />
+      </div>
+      <h2>Cartoon Name: Doremon </h2>
+      <h3>Rating:8.2</h3>
+      <p>Summary: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque qui laborum explicabo dignissimos, illum, repellendus neque impedit ab esse iste assumenda at quisquam, quo debitis.</p>
+    </div>
+  );
+}
