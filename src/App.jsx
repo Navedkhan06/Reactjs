037f5802e5d497a50a63d6698c9988889a9f2cdd
Navedkhan06 <nkhan25790@gmail.com>
2025-10-05 20:29:24 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import CartoonSeries from "./components/CartoonSeries";
 // import "./components/Cartoon.css";
 import "./components/Cartoon.module.css";
-import { EventHandling } from "./components/EventHandling";
+// import { EventHandling } from "./components/EventHandling";
+// import { EventProps } from "./components/EventProps";
+// import { EventPropagation } from "./components/EventPropagation";
 
+import { Sibling, State } from "./components/hooks/State";
 export const App = () => {
   return (
     <section className="container">
-      <h1 className="card-heading">List of Best Cartoon Series</h1>
+      {/* <h1 className="card-heading">List of Best Cartoon Series</h1> */}
       {/* <CartoonSeries />; */}
-      <EventHandling />
+      {/* <EventHandling /> */}
+      {/* <EventProps /> */}
+      {/* <EventPropagation /> */}
+      <State />
+      <Sibling/>
     </section>
   )
 };
