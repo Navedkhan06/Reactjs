df056f56d5a72a4955ede13fb9b6a566af30a347
Navedkhan06 <nkhan25790@gmail.com>
2025-10-07 15:16:10 +0530

    Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { DerivedState } from "./components/DerivedState";
-import {State} from "./components/hooks/State"
+// import { DerivedState } from "./components/DerivedState";
+// import {State} from "./components/hooks/State"
+import { LiftingState } from "./components/hooks/LiftStateUp";
 
 export const App = () => {
   return (
     <section className="container">
       {/* <State />  */}
-      <DerivedState />
+      {/* <DerivedState /> */}
+      <LiftingState />
     </section>
   );
 };
