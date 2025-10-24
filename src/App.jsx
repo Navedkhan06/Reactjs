1a7a14386858c40b060c2db0dce8507900cad901
Navedkhan06 <nkhan25790@gmail.com>
2025-10-24 18:20:51 +0530

    ToggleSwitch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 // import { DerivedState } from "./components/DerivedState";
 // import {State} from "./components/hooks/State"
-import { LiftingState } from "./components/hooks/LiftStateUp";
-
+// import { LiftingState } from "./components/hooks/LiftStateUp";
+import { ToggleSwitch } from "./projects/ToggleSwitch/ToggleSwitch";
 export const App = () => {
   return (
     <section className="container">
       {/* <State />  */}
       {/* <DerivedState /> */}
-      <LiftingState />
+      {/* <LiftingState /> */}
+      <ToggleSwitch/>
     </section>
   );
 };
